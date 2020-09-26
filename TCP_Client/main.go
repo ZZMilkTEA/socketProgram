a070f9d68bc4b4c1c01ab1be093563c5d3c6cd2b
ZZMilkTEA <504267031@qq.com>
2020-09-27 00:54:34 +0800

    feature: 使TCP的服务端在客户端断连时可以断开此次连接

--- TCP_Client/main.go
+++ TCP_Client/main.go
@@ -10,30 +10,31 @@ import (
 
 //客户端
 
-func main()  {
-	conn ,err := net.Dial("tcp","127.0.0.1:8888")
+func main() {
+	conn, err := net.Dial("tcp", "127.0.0.1:8888")
 	fmt.Println("准备与服务端连接")
 	if err != nil {
-		fmt.Println("连接失败，错误: ",err)
+		fmt.Println("连接失败，错误: ", err)
 		return
 	}
 	defer conn.Close()
 	fmt.Println("连接成功，请随便输入些什么,输入q退出")
 	inputReader := bufio.NewReader(os.Stdin)
 	for {
-		input, _ := inputReader.ReadString('\n')    //读取用户输入
-		inputInfo := strings.Trim(input,"\r\n")
-		if strings.ToUpper(inputInfo) == "Q"{
-			return  //如果输入q就退出
+		input, _ := inputReader.ReadString('\n') //读取用户输入
+		inputInfo := strings.Trim(input, "\r\n")
+		if strings.ToUpper(inputInfo) == "Q" {
+			_, err = conn.Write([]byte(inputInfo))
+			return //如果输入q就退出
 		}
-		_,err = conn.Write([]byte(inputInfo))   //发送数据
-		if err != nil{
+		_, err = conn.Write([]byte(inputInfo)) //发送数据
+		if err != nil {
 			return
 		}
 		buf := [512]byte{}
-		n,err := conn.Read(buf[:])
-		if err != nil{
-			fmt.Println("接受失败，错误: ",err)
+		n, err := conn.Read(buf[:])
+		if err != nil {
+			fmt.Println("接受失败，错误: ", err)
 			return
 		}
 		fmt.Println(string(buf[:n]))
--- TCP_Server/main.go
+++ TCP_Server/main.go
@@ -4,39 +4,44 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"strings"
 )
 
 //TCP server端
 
-func process(conn net.Conn)  {
-	defer conn.Close()  //关闭连接
+func process(conn net.Conn) {
+	defer conn.Close() //关闭连接
 	fmt.Println("准备接收数据……")
 	for {
 		reader := bufio.NewReader(conn)
 		var buf [128]byte
-		n,err := reader.Read(buf[:])    //读取数据
-		if err != nil{
-			fmt.Println("连接客户端失败,错误信息：",err)
+		n, err := reader.Read(buf[:]) //读取数据
+		if err != nil {
+			fmt.Println("连接客户端失败,错误信息：", err)
 		}
 		recvStr := string(buf[:n])
-		fmt.Println("收到客户端信息:  ",recvStr)
+		fmt.Println("收到客户端信息:  ", recvStr)
+		if strings.ToUpper(recvStr) == "Q" {
+			fmt.Println("客户退出，与客户断开连接")
+			return
+		}
 		conn.Write([]byte("服务端刚刚收到您的信息为:  " + recvStr)) //发送数据
 	}
 }
-func main()  {
-	listen,err := net.Listen("tcp","127.0.0.1:8888")
-	if err != nil{
-		fmt.Println("监听失败，错误：",err)
+func main() {
+	listen, err := net.Listen("tcp", "127.0.0.1:8888")
+	if err != nil {
+		fmt.Println("监听失败，错误：", err)
 		return
 	}
 	fmt.Println("监听中……")
 	for {
-		conn,err := listen.Accept() //建立连接
-		if err!= nil{
-			fmt.Println("建立连接失败，错误：",err)
+		conn, err := listen.Accept() //建立连接
+		if err != nil {
+			fmt.Println("建立连接失败，错误：", err)
 			continue
 		}
 		fmt.Println("已经与客户端连接")
-		go process(conn)    //启动一个goroutine处理连接,使服务器具备并发处理请求的能力
+		go process(conn) //启动一个goroutine处理连接,使服务器具备并发处理请求的能力
 	}
 }
